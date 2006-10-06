449ffccd06dde4768cfc0a13aaf52d64e0b9c1e1
Peter Hinrichsen <peter_hinrichsen@techinsite.com.au>
2006-10-06 06:31:38 +0000

    Build: Updated SVN-Checkout-tiOPF.bat
    git-svn-id: https://tiopf.svn.sourceforge.net/svnroot/tiopf/tiOPF2/Trunk@86 68417047-fa15-0410-9c44-b939d5e0afe3

--- InstallScripts/SVN-Checkout-tiOPF.bat
+++ InstallScripts/SVN-Checkout-tiOPF.bat
@@ -2,18 +2,18 @@ rem * You will need to install the subversion command line client
 rem * from http://subversion.tigris.org/
 rem *
 rem * If you are behind a proxy server, you will need a file
-rem * called servers in the \Subversion\bin directory containing
+rem * called servers in the "Your private directory\Subversion\bin" directory containing
 rem * the following text:
 rem * 
 rem * [groups]
-rem * tiopf = tiopf.techinsite.com.au
+rem * sourceforge = svn.sourceforge.net
 rem * 
-rem * [tiopf]
+rem * [sourceforge]
 rem * http-proxy-host = enter-value-here
 rem * http-proxy-port = enter-value-here
 
 set path_to_svn="C:\Program Files\Subversion\bin\svn" 
-set svn_url_tiopf=http://tiopf.techinsite.com.au/svn/tiopf
+set svn_url_tiopf=https://svn.sourceforge.net/svnroot/tiopf/tiOPF2/trunk
 set check_out_to=C:\temp\tiOPF2
 
 %path_to_svn% co %svn_url_tiopf% %check_out_to%
