0db8a31b011a138bf955dde5c76cb2346f865d16
Peter Hinrichsen <peter_hinrichsen@techinsite.com.au>
2007-07-23 03:28:25 +0000

    Demos: Added Demo_10a_CollectionWithAutoFilter
    git-svn-id: https://tiopf.svn.sourceforge.net/svnroot/tiopf/tiOPF2/Trunk@674 68417047-fa15-0410-9c44-b939d5e0afe3

---
+++ Demos/Demo_10a_CollectionWithAutoFilter/Client_BOM.pas
@@ -0,0 +1,49 @@
+unit Client_BOM;
+
+interface
+uses
+  tiObject
+  ,tiFilteredObjectList
+ ;
+
+type
+
+  TClient = class;
+  TClients = class;
+
+  TClientName = String[200];
+  TClientID   = String[9];
+
+  TClients = class(TtiFilteredObjectList);
+
+  TClient = class(TtiObject)
+  private
+    FClientID: TClientID;
+    FClientName: TClientName;
+  public
+    constructor CreateNew(const pDatabaseName: string = ''; const pPerLayerName: string = ''); override;
+  published
+    property    ClientName: TClientName read FClientName write FClientName;
+    property    ClientID  : TClientID read FClientID write FClientID;
+  end;
+
+implementation
+uses
+   tiOPFManager
+  ,SysUtils
+  ,Windows
+ ;
+
+{ TClient }
+
+constructor TClient.CreateNew(const pDatabaseName: string = ''; const pPerLayerName: string = '');
+begin
+  inherited;
+  // Set some default values for the demo
+  ClientID:= IntToStr(GetTickCount);
+
+  ClientName:= Char(GetTickCount mod 26 + ord('A')) + DateTimeToStr(Now);
+end;
+
+end.
+
