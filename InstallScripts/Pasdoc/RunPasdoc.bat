92cae2ebb126076699273b83eaec6efa16224c78
Peter Hinrichsen <peter_hinrichsen@techinsite.com.au>
2007-08-08 05:53:33 +0000

    Doc: Added pasdoc script
    git-svn-id: https://tiopf.svn.sourceforge.net/svnroot/tiopf/tiOPF2/Trunk@740 68417047-fa15-0410-9c44-b939d5e0afe3

--- InstallScripts/Pasdoc/RunPasdoc.bat
+++ InstallScripts/Pasdoc/RunPasdoc.bat
@@ -4,6 +4,6 @@ set target_directory=Doc
 
 cd %source_directory%
 if not exist %target_directory% md %target_directory%
-%pasdoc% -S InstallScripts\Pasdoc\FilesToDocument.txt -O html -I Core -E %target_directory%
+%pasdoc% -S InstallScripts\Pasdoc\FilesToDocument.txt -O html -I Core -E %target_directory% --staronly
 
 pause
