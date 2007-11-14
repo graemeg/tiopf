ce42a7fa014873f1f62d151b91147a7655f214a0
Peter Hinrichsen <peter_hinrichsen@techinsite.com.au>
2007-11-14 11:41:57 +0000

    Unit tests: Added tiApplicationServer_TST.pas
    git-svn-id: https://tiopf.svn.sourceforge.net/svnroot/tiopf/tiOPF2/Trunk@924 68417047-fa15-0410-9c44-b939d5e0afe3

---
+++ UnitTests/Tests/tiApplicationServer_TST.pas
@@ -0,0 +1,75 @@
+unit tiApplicationServer_TST;
+
+{$I tiDefines.inc}
+
+interface
+uses
+   tiTestFrameWork
+ ;
+
+type
+
+  TTestTIApplicationServer = class(TtiTestCase)
+  published
+    procedure tiDBProxyServer_Create;
+    procedure tiDBProxyServer_ServerVersion;
+    procedure tiDBProxyServer_ExecuteRemoteXML;
+    procedure tiDBProxyServer_TestAlive1;
+    procedure tiDBProxyServer_TestHTML;
+    procedure tiDBProxyServer_TestXML;
+    procedure tiDBProxyServer_TestAlive;
+  end;
+
+procedure RegisterTests;
+
+implementation
+uses
+  tiTestDependencies
+  ;
+
+procedure RegisterTests;
+begin
+  RegisterNonPersistentTest(TTestTIApplicationServer);
+end;
+
+const
+  cPort= 81;
+
+{ TTestTIWebServer }
+
+procedure TTestTIApplicationServer.tiDBProxyServer_Create;
+begin
+
+end;
+
+procedure TTestTIApplicationServer.tiDBProxyServer_ExecuteRemoteXML;
+begin
+
+end;
+
+procedure TTestTIApplicationServer.tiDBProxyServer_ServerVersion;
+begin
+
+end;
+
+procedure TTestTIApplicationServer.tiDBProxyServer_TestAlive;
+begin
+
+end;
+
+procedure TTestTIApplicationServer.tiDBProxyServer_TestAlive1;
+begin
+
+end;
+
+procedure TTestTIApplicationServer.tiDBProxyServer_TestHTML;
+begin
+
+end;
+
+procedure TTestTIApplicationServer.tiDBProxyServer_TestXML;
+begin
+
+end;
+
+end.
