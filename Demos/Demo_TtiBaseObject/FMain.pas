6106e865596eb5c7d4a06c2d712d6063d184281e
Peter Hinrichsen <peter_hinrichsen@techinsite.com.au>
2007-11-16 07:12:14 +0000

    Demos: Added Demo_TtiBaseObject
    git-svn-id: https://tiopf.svn.sourceforge.net/svnroot/tiopf/tiOPF2/Trunk@937 68417047-fa15-0410-9c44-b939d5e0afe3

---
+++ Demos/Demo_TtiBaseObject/FMain.pas
@@ -0,0 +1,120 @@
+unit FMain;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    GroupBox1: TGroupBox;
+    NoReferenceCountingPerformance: TButton;
+    btnReferenceCountingPerformance: TButton;
+    GroupBox2: TGroupBox;
+    btnTestValid: TButton;
+    Memo1: TMemo;
+    memoLog: TMemo;
+    procedure FormCreate(Sender: TObject);
+    procedure PerformanceTestNoReferenceCounting(Sender: TObject);
+    procedure PerformanceTestReferenceCounting(Sender: TObject);
+    procedure btnTestValidClick(Sender: TObject);
+  private
+    procedure Log(const AMessage: string);
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+uses
+   tiBaseObject
+  ,tiUtils
+  ;
+
+const
+  CTestRunTime = 5; // Seconds
+
+{$R *.dfm}
+
+procedure TForm1.PerformanceTestNoReferenceCounting(Sender: TObject);
+var
+  LO: TtiBaseObject;
+  LStart: Cardinal;
+  LCount: Cardinal;
+  i: integer;
+begin
+  LCount:= 0;
+  LStart:= tiGetTickCount;
+  while tiGetTickCount - LStart < CTestRunTime * 1000 do
+  begin
+    for i := 1 to 10000 do
+    begin
+      LO:= TtiBaseObject.Create;
+      LO.Free;
+    end;
+    Inc(LCount);
+  end;
+  Log(Format('%s iterations in %d seconds (no reference counting)',
+    [tiIntToCommaStr(LCount), CTestRunTime]));
+end;
+
+procedure TForm1.PerformanceTestReferenceCounting(Sender: TObject);
+{$ifdef reference_counting}
+var
+  LO: IInterface;
+  LStart: Cardinal;
+  LCount: Cardinal;
+  i: integer;
+begin
+  LCount:= 0;
+  LStart:= tiGetTickCount;
+  while tiGetTickCount - LStart < CTestRunTime * 1000 do
+  begin
+    for i := 1 to 10000 do
+      LO:= TtiBaseObject.CreateWithRefCounting;
+    Inc(LCount);
+  end;
+  Log(Format('%s iterations in %d seconds (reference counting)',
+    [tiIntToCommaStr(LCount), CTestRunTime]));
+{$else}
+begin
+  Assert(False, 'reference_counting not enabled');
+{$endif}
+end;
+
+procedure TForm1.btnTestValidClick(Sender: TObject);
+var
+  LO: TtiBaseObject;
+begin
+  LO:= TtiBaseObject.Create;
+  Assert(LO.TestValid);
+  LO.Free;
+  Assert(not LO.TestValid);
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  {$ifdef object_tracking}
+    Log('object_tracking is ON');
+  {$else}
+    btnTestValid.Enabled:= False;
+    Log('object_tracking is OFF');
+  {$endif}
+  {$ifdef reference_counting}
+    Log('reference_counting is ON');
+  {$else}
+    Log('reference_counting is OFF');
+    btnReferenceCountingPerformance.Enabled:= False;
+  {$endif}
+  Log('---');
+end;
+
+procedure TForm1.Log(const AMessage: string);
+begin
+  memoLog.Lines.Add(AMessage);
+end;
+
+end.
---
+++ Demos/Demo_TtiBaseObject/tiBaseObjectPerformanceTesting.dpr
@@ -0,0 +1,14 @@
+program tiBaseObjectPerformanceTesting;
+
+uses
+  Forms,
+  FMain in 'FMain.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
