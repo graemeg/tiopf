788054317d308b8b5d92a71c8e4dc0d97b998cf8
Peter Hinrichsen <peter_hinrichsen@techinsite.com.au>
2008-04-07 06:37:28 +0000

    AdrsBook Demo - work in progress
    git-svn-id: https://tiopf.svn.sourceforge.net/svnroot/tiopf/tiOPF2/Trunk@1194 68417047-fa15-0410-9c44-b939d5e0afe3

--- Demos/WorkInProgress/Demo_30_AdrsBookSimple/Data/RunCreateFirebirdDB.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set PathToISQL="C:\Program Files\Firebird\Firebird_1_5\bin\isql.exe"
-set ScriptName=CreateFirebirdDB.sql
-
-erase AdrsBookSimple.fdb
-%PathToISQL% -i %ScriptName%
-
-pause
