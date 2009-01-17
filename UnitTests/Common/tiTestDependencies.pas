133a82809141fd5722e07983a4a7efb703ea3b25
Peter Hinrichsen <peter_hinrichsen@techinsite.com.au>
2009-01-17 10:18:46 +0000

    Changed persistence layers to test in attempt to fix broken build.
    git-svn-id: https://tiopf.svn.sourceforge.net/svnroot/tiopf/tiOPF2/Trunk@1450 68417047-fa15-0410-9c44-b939d5e0afe3

--- UnitTests/Common/tiTestDependencies.pas
+++ UnitTests/Common/tiTestDependencies.pas
@@ -122,10 +122,11 @@ begin
   tiBaseObject_TST.RegisterTests;
   tiUtils_TST.RegisterTests;
   tiRTTI_TST.RegisterTests;
-  tiPersistenceLayers_TST.RegisterTests;
-  tiVisitor_TST.RegisterTests;
-  tiVisitorDB_TST.RegisterTests;
-  tiObject_TST.RegisterTests;
+//  tiPersistenceLayers_TST.RegisterTests;
+//  tiVisitor_TST.RegisterTests;
+//  tiVisitorDB_TST.RegisterTests;
+
+//  tiObject_TST.RegisterTests;
 //  tiCompress_TST.RegisterTests;
 //  tiEncrypt_TST.RegisterTests;
 //  tiStreams_TST.RegisterTests;
