ec52bfdf617ac8d3029a655fbaa7a92e36a590b0
Graeme Geldenhuys <graemeg@gmail.com>
2009-04-18 08:46:19 +0000

    * Added missing units to tiOPFLCL.lpk package for use with Lazarus IDE.
    git-svn-id: https://tiopf.svn.sourceforge.net/svnroot/tiopf/tiOPF2/Trunk@1503 68417047-fa15-0410-9c44-b939d5e0afe3

--- Compilers/FPC/tiopflcl.pas
+++ Compilers/FPC/tiopflcl.pas
@@ -1,4 +1,4 @@
-{ This file was automatically created by Lazarus. do not edit!
+{ This file was automatically created by Lazarus. do not edit ! 
   This source is only used to compile and install the package.
  }
 
@@ -7,7 +7,7 @@ unit tiopfLCL;
 interface
 
 uses
-tiMediators, tiListMediators;
+    tiMediators, tiListMediators, tiGUIINI, tiDialogs, tiGUIConstants, tiGUIUtils, tiLogToGUI;
 
 implementation
 
