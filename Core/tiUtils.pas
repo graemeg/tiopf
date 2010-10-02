caa3fe0c09fe05c2efc3684a6663ccf299e4deda
Graeme Geldenhuys <graemeg@gmail.com>
2010-10-02 14:08:36 +0000

    TtiIntegerList.Sort bugfix. Thanks to "G.J. Doornink" <dkg@xs4all.nl> for spotting it.
    * Fixed the bug
    * Also added a unit test for TtiIntegerList.Sort functionality
    
    git-svn-id: https://tiopf.svn.sourceforge.net/svnroot/tiopf/tiOPF2/Trunk@1840 68417047-fa15-0410-9c44-b939d5e0afe3

--- Core/tiUtils.pas
+++ Core/tiUtils.pas
@@ -4044,7 +4044,7 @@ function _DoSort(Item1, Item2: Pointer): Integer;
 begin
   result:=
     CompareValue(
-      TtiIntegerListItem(Item2).AValue,
+      TtiIntegerListItem(Item1).AValue,
       TtiIntegerListItem(Item2).AValue);
 end;
 
