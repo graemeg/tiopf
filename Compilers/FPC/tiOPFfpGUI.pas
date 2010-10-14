5e95c8877154a6e519ec280067603533d6dc2199
Graeme Geldenhuys <graemeg@gmail.com>
2010-10-14 09:46:09 +0000

    Add the tiOPF + fpGUI Toolkit package.
    The unit output directory is set so that it will not conflict
    with the tiOPF + LCL package, where units have the same names.
    
    git-svn-id: https://tiopf.svn.sourceforge.net/svnroot/tiopf/tiOPF2/Trunk@1918 68417047-fa15-0410-9c44-b939d5e0afe3

---
+++ Compilers/FPC/tiOPFfpGUI.pas
@@ -0,0 +1,15 @@
+{ This file was automatically created by Lazarus. do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit tiOPFfpGUI; 
+
+interface
+
+uses
+  tiGUIUtils, tiDialogs, tiGUIINI, tiGUIConstants, tiRtfReport, tiMediators, 
+  tiListMediators, tiLogToGUI;
+
+implementation
+
+end.
