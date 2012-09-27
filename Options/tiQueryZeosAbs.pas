99d4f5b1cb7868c572c2708618c97b1eab2d6862
Graeme Geldenhuys <graemeg@gmail.com>
2012-09-27 11:26:34 +0000

    Refactored ZeosAbs persistence layer unit  to subclass TtiQueryDataset instead of TtiQuerySQL
    The reduces the code duplication considerably, and simplifies the Zeos implementation.
    Thanks to Raul Ferriz <raul.ferriz@gmail.com> for the contribution.
    
    git-svn-id: https://tiopf.svn.sourceforge.net/svnroot/tiopf/tiOPF2/Trunk@2224 68417047-fa15-0410-9c44-b939d5e0afe3

--- Options/tiQueryZeosAbs.pas
+++ Options/tiQueryZeosAbs.pas
@@ -5,6 +5,7 @@ unit tiQueryZeosAbs;
 interface
 uses
    tiQuery
+  ,tiQueryDataset
   ,Classes
   ,ZConnection
   ,ZDataset
@@ -24,91 +25,40 @@ type
   public
     constructor Create ; override ;
     destructor  Destroy ; override ;
-
     procedure   StartTransaction ; override ;
     function    InTransaction : boolean ; override ;
     procedure   Commit ; override ;
     procedure   RollBack ; override ;
     function    Test : boolean; override;
     function    TIQueryClass: TtiQueryClass; override;
+    property    Connection : TZConnection read FConnection write FConnection ;
+  end;
 
-    property  Connection : TZConnection read FConnection write FConnection ;    
-  end ;
 
-
-  TtiQueryZeos = class( TtiQuerySQL )
+  TtiQueryZeos = class( TtiQueryDataset )
   private
     FQuery : TZQuery ;
+    procedure Prepare;
   protected
-
-    function    GetFieldAsString(const psName: string): string   ; override ;
-    function    GetFieldAsFloat(const psName: string): extended  ; override ;
-    function    GetFieldAsBoolean(const psName: string): boolean    ; override ;
-    function    GetFieldAsInteger(const psName: string): Int64 ; override ;
-    function    GetFieldAsDateTime(const psName: string):TDateTime ; override ;
-
-    function    GetFieldAsStringByIndex(pIndex: Integer): string     ; override ;
-    function    GetFieldAsFloatByIndex(pIndex: Integer)   : extended ; override ;
-    function    GetFieldAsBooleanByIndex(pIndex: Integer) : boolean  ; override ;
-    function    GetFieldAsIntegerByIndex(pIndex: Integer) : Int64    ; override ;
-    function    GetFieldAsDateTimeByIndex(pIndex: Integer):TDateTime ; override ;
-    function    GetFieldIsNullByIndex(pIndex: Integer):Boolean       ; override ;
-
+    procedure   CheckPrepared; override;
     function    GetSQL: TStrings; override ;
     procedure   SetSQL(const Value: TStrings); override ;
-    function    GetActive: boolean; override ;
     procedure   SetActive(const Value: boolean); override ;
-    function    GetEOF: boolean; override ;
-    function    GetParamAsString( const psName: string): string; override ;
-    function    GetParamAsBoolean(const psName: string): boolean; override ;
-    function    GetParamAsFloat(const psName: string): extended;override ;
-    function    GetParamAsInteger(const psName: string): Int64 ;override ;
-    procedure   SetParamAsString( const psName, Value: string); override ;
-    procedure   SetParamAsBoolean(const psName: string;const Value: boolean);override ;
-    procedure   SetParamAsFloat(const psName: string; const Value: extended);override ;
-    procedure   SetParamAsInteger(const psName: string;const Value: Int64);override ;
-    function    GetParamAsDateTime(const psName: string): TDateTime ; override ;
-    procedure   SetParamAsDateTime(const psName :string ; const Value: TDateTime); override ;
-    function    GetParamAsTextBLOB(const psName: string): string; override ;
-    procedure   SetParamAsTextBLOB(const psName, Value: string); override ;
-
-    function    GetParamIsNull( const psName: String): Boolean; override;
-    procedure   SetParamIsNull( const psName: String; const Value: Boolean); override;
-    function    GetFieldIsNull(const psName: string): Boolean; override ;
-
   public
     constructor Create ; override ;
     destructor  Destroy ; override ;
-    procedure   Open    ; override ;
-    procedure   Close   ; override ;
-    procedure   Next    ; override ;
     function    ExecSQL: integer; override;
-
-    function    ParamCount : integer ; override ;
-    function    ParamName( pIndex : integer ) : string ; override ;
-
-    procedure   AssignParamToStream( const pName : string ; const pStream : TStream ) ; override ;
-    procedure   AssignParamFromStream( const pName : string ; const pStream : TStream ) ; override ;
-    procedure   AssignFieldAsStream( const pName : string ; const pStream : TStream ) ; override ;
-    procedure   AssignFieldAsStreamByIndex(      pIndex : integer ; const pValue : TStream ) ; override ;
-
     procedure   AttachDatabase( pDatabase : TtiDatabase ) ; override ;
     procedure   DetachDatabase ;  override ;
     procedure   Reset ; override ;
-
-    function    FieldCount : integer ; override ;
-    function    FieldName( pIndex : integer ) : string ; override ;
-    function    FieldIndex( const psName : string ) : integer ; override ;
-    function    FieldKind( pIndex : integer ) : TtiQueryFieldKind ; override ;
-    function    FieldSize( pIndex : integer ) : integer ; override ;
     function    HasNativeLogicalType : boolean ; override ;
-
-  end ;
+  end;
 
 
 implementation
 uses
    tiDBConnectionPool
+  ,tiConstants
   ,tiLog
   ,tiUtils
   ,tiOPFManager
@@ -128,149 +78,56 @@ constructor TtiQueryZeos.Create;
 begin
   inherited;
   FQuery := TZQuery.Create( nil ) ;
+  Dataset := FQuery;
+  Params  := FQuery.Params;
+  FSupportsRowsAffected := True;
 end;
 
 destructor TtiQueryZeos.Destroy;
 begin
+  Params := nil;
+  Dataset := nil;
   FQuery.Free ;
-  inherited;
-end;
-
-procedure TtiQueryZeos.Close;
-begin
-  Active := false ;
+  inherited Destroy;
 end;
 
 function TtiQueryZeos.ExecSQL: integer;
 begin
+  Prepare;
+  LogParams;
   FQuery.ExecSQL;
-  Result := -1
-  { TODO : When implementing RowsAffected, please return correct result
-           and put FSupportsRowsAffected := True; in TtiQueryXXX.Create; }
-
+  Result := FQuery.RowsAffected;
 end;
 
-function TtiQueryZeos.GetFieldAsBoolean(const psName: string): boolean;
-var
-  lsValue : string ;
+procedure TtiQueryZeos.Prepare;
 begin
-  lsValue := upperCase( FQuery.FieldByName( psName ).AsString ) ;
-  result := ( lsValue = 'T'    ) or
-            ( lsValue = 'TRUE' ) or
-            ( lsValue = 'Y'    ) or
-            ( lsValue = 'YES'  ) or
-            ( lsValue = '1'    ) ;
+  if FQuery.Prepared then
+    Exit; // ==>
+  FQuery.Prepare;
 end;
 
-function TtiQueryZeos.GetFieldAsDateTime(const psName: string): TDateTime;
+procedure TtiQueryZeos.CheckPrepared;
 begin
-  result := FQuery.FieldByName( psName ).AsDateTime ;
-end;
-
-function TtiQueryZeos.GetFieldAsFloat(const psName: string): extended;
-begin
-  result := FQuery.FieldByName( psName ).AsFloat ;
-end;
-
-function TtiQueryZeos.GetFieldAsInteger(const psName: string): Int64;
-begin
-  result := FQuery.FieldByName( psName ).AsInteger ;
-end;
-
-function TtiQueryZeos.GetFieldAsString(const psName: string): string;
-begin
-  result := FQuery.FieldByName(psName).AsString;
-end;
-
-function TtiQueryZeos.GetActive: boolean;
-begin
-  result := FQuery.Active ;
-end;
-
-function TtiQueryZeos.GetEOF: boolean;
-begin
-  result := FQuery.EOF ;
-end;
-
-function TtiQueryZeos.GetParamAsBoolean(const psName: string): boolean;
-begin
-  result := FQuery.ParamByName( psName ).AsBoolean ;
-end;
-
-function TtiQueryZeos.GetParamAsDateTime(const psName: string): TDateTime;
-begin
-  result := FQuery.ParamByName( psName ).AsDateTime ;
-end;
-
-function TtiQueryZeos.GetParamAsFloat(const psName: string): extended;
-begin
-  result := FQuery.ParamByName( psName ).AsFloat ;
-end;
-
-function TtiQueryZeos.GetParamAsInteger(const psName: string): Int64;
-begin
-  result := FQuery.ParamByName( psName ).AsInteger ;
-end;
-
-function TtiQueryZeos.GetParamAsString(const psName: string): string;
-begin
-  result := FQuery.ParamByName(psName).AsString;
+  inherited CheckPrepared;
+  Prepare;
 end;
 
 function TtiQueryZeos.GetSQL: TStrings;
 begin
-  result := FQuery.SQL ;
-end;
-
-procedure TtiQueryZeos.Next;
-begin
-  FQuery.Next ;
-end;
-
-procedure TtiQueryZeos.Open;
-begin
-  Active := true ;
-end;
-
-function TtiQueryZeos.ParamCount: integer;
-begin
-  result := FQuery.Params.Count;
-end;
-
-function TtiQueryZeos.ParamName(pIndex: integer): string;
-begin
-  result := FQuery.Params.Items[ pIndex ].Name ;
+  Result := FQuery.SQL ;
 end;
 
 procedure TtiQueryZeos.SetActive(const Value: boolean);
 begin
-  FQuery.Active := Value ;
-end;
-
-procedure TtiQueryZeos.SetParamAsBoolean(const psName: string; const Value: boolean);
-begin
-  FQuery.ParamByName( psName ).AsBoolean := Value ;
-end;
-
-procedure TtiQueryZeos.SetParamAsDateTime(const psName : string ; const Value: TDateTime);
-begin
-  FQuery.ParamByName( psName ).AsDateTime := Value ;
-end;
-
-procedure TtiQueryZeos.SetParamAsFloat(const psName: string;
-  const Value: extended);
-begin
-  FQuery.ParamByName( psName ).AsFloat := Value ;
-end;
-
-procedure TtiQueryZeos.SetParamAsInteger(const psName: string; const Value: Int64);
-begin
-  FQuery.ParamByName( psName ).AsInteger := Value ;
-end;
-
-procedure TtiQueryZeos.SetParamAsString(const psName, Value: string);
-begin
-  FQuery.ParamByName(UpperCase(psName)).AsString := Value;
+  Assert(Database.TestValid(TtiDatabase), 'Database is not valid');
+  if Value then
+  begin
+    FQuery.Open;
+  end
+  else
+  begin
+    FQuery.Close;
+  end;
 end;
 
 procedure TtiQueryZeos.SetSQL(const Value: TStrings);
@@ -278,46 +135,28 @@ begin
   FQuery.SQL.Assign( Value ) ;
 end;
 
-procedure TtiQueryZeos.AssignFieldAsStream(const pName: string; const pStream: TStream );
-var
-  lField : TField ;
-begin
-  lField := FQuery.FieldByName(pName);
-  Assert(lField is TBlobField, 'Field <' + pName + '> not a TBlobField' ) ;
-  ( lField as TBlobField ).SaveToStream(pStream);
-end;
-
 procedure TtiQueryZeos.AttachDatabase(pDatabase: TtiDatabase);
 begin
-  FQuery.Connection := TtiDatabaseZeosAbs( pDatabase ).Connection;
-  Database := pDatabase ;
+  inherited AttachDatabase(pDatabase);
+  if (pDatabase is TtiDatabaseZeosAbs) then
+  begin
+    FQuery.Connection := TtiDatabaseZeosAbs( pDatabase ).Connection;
+  end;
 end;
 
 procedure TtiQueryZeos.DetachDatabase;
 begin
-  inherited DetachDatabase ;
-end;
-
-function TtiQueryZeos.FieldCount: integer;
-begin
-  result := FQuery.FieldCount ;
-end;
-
-function TtiQueryZeos.FieldName(pIndex: integer): string;
-begin
-  result := FQuery.Fields[pIndex].FieldName ;
+  inherited DetachDatabase;
+  if FQuery.Active then
+     FQuery.Close;
+  FQuery.Connection := Nil;
 end;
 
 procedure TtiQueryZeos.Reset;
 begin
-  Active := false ;
-  FQuery.SQL.Clear ;
-  FQuery.Params.Clear ;
-end;
-
-function TtiQueryZeos.FieldIndex(const psName: string): integer;
-begin
-  result := FQuery.FieldByName( psName ).Index ;
+  Active := False;
+  FQuery.SQL.Clear;
+  //FQuery.Params.Clear;
 end;
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
@@ -364,78 +203,6 @@ begin
   FConnection.StartTransaction ;
 end;
 
-// This code is cloned in TtiQueryIB - Looks like we need to abstract more
-// and introduce a TDataSet version of the TtiQuery
-function TtiQueryZeos.FieldKind(pIndex: integer): TtiQueryFieldKind;
-var
-  lDataType : TFieldType ;
-begin
-  lDataType := FQuery.Fields[ pIndex ].DataType ;
-
-  // These are the available field types for a TDataSet descendant
-//  TFieldType = (ftUnknown, ftString, ftSmallint, ftInteger, ftWord,
-//    ftBoolean, ftFloat, ftCurrency, ftBCD, ftDate, ftTime, ftDateTime,
-//    ftBytes, ftVarBytes, ftAutoInc, ftBlob, ftMemo, ftGraphic, ftFmtMemo,
-//    ftParadoxOle, ftDBaseOle, ftTypedBinary, ftCursor, ftFixedChar, ftWideString,
-//    ftLargeint, ftADT, ftArray, ftReference, ftDataSet, ftOraBlob, ftOraClob,
-//    ftVariant, ftInterface, ftIDispatch, ftGuid);
-
-  // These are the available TtiQueryFieldKind(s)
-//  ,
-//  qfkInteger,
-//  qfkFloat,
-//  qfkDateTime,
-//  qfkLogical,
-//  qfkBinary,
-//  qfkMacro,
-//  qfkLongString
-
-    case lDataType of
-    ftString, ftWideString :                    result := qfkString   ;
-    ftSmallint, ftInteger, ftWord, ftLargeint : result := qfkInteger  ;
-//    ftBoolean :                                 result := qfkLogical  ;
-    ftFloat, ftCurrency, ftBCD :                result := qfkFloat    ;
-    ftDate, ftTime, ftDateTime :                result := qfkDateTime ;
-    ftBlob{, ftGraphic} :                       result := qfkBinary   ;
-    ftMemo, ftFmtMemo :                         result := qfkLongString ;
-    else
-      raise EtiOPFInternalException.Create(
-        'Invalid FQuery.Fields[ pIndex ].DataType <' +
-                      GetEnumName( TypeInfo( TFieldType ), Ord( lDataType )) + '>') ;
-    end ;
-//    ftUnknown,
-//    ftBytes, ftVarBytes, ftAutoInc,
-//    ftParadoxOle, ftDBaseOle, ftTypedBinary, ftCursor, ftFixedChar,
-//    ftADT, ftArray, ftReference, ftDataSet, ftOraBlob, ftOraClob,
-//    ftVariant, ftInterface, ftIDispatch, ftGuid
-
-end;
-
-function TtiQueryZeos.FieldSize(pIndex: integer): integer;
-begin
-  case FieldKind( pIndex ) of
-    qfkString     : result := FQuery.FieldDefs[ pIndex ].Size ;
-    qfkLongString : result := 0 ;
-    qfkInteger    : result := 0 ;
-    qfkFloat      : result := 0 ;
-    qfkDateTime   : result := 0 ;
-    qfkBinary     : result := 0 ;
- //   qfkLogical    : result := 0 ;
-  else
-    raise EtiOPFInternalException.Create( 'Invalid field type') ;
-  end ;
-end;
-
-function TtiQueryZeos.GetParamIsNull(const psName: String): Boolean;
-begin
-  result := FQuery.ParamByName( psName ).IsNull ;
-end;
-
-procedure TtiQueryZeos.SetParamIsNull(const psName: String; const Value: Boolean);
-begin
-  FQuery.ParamByName( psName ).Clear ;
-end;
-
 function TtiDatabaseZeosAbs.GetConnected: boolean;
 begin
   Result := FConnection.Connected ;
@@ -458,29 +225,15 @@ begin
   try
     FConnection.Connected := true ;
   except
+    on e: EDatabaseError do
+    begin
+      lsErrorMessage := 'Error attempting to connect to database.' + Cr + e.Message;
+      raise EtiOPFDBExceptionUserNamePassword.Create(
+        cTIPersistZeosFB, DatabaseName, UserName, Password, lsErrorMessage);
+    end;
     on e:exception do
       raise EtiOPFDBExceptionCanNotConnect.Create( 'Unknown', DatabaseName, UserName, Password, e.message ) ;
   end ;
-end;
-
-function TtiQueryZeos.GetFieldIsNull(const psName: string): Boolean;
-begin
-  result := FQuery.FieldByName( psName ).IsNull ;
-end;
-
-procedure TtiQueryZeos.AssignParamToStream(const pName: string; const pStream : TStream);
-var
-  ls : string ;
-begin
-  Assert( pStream <> nil, 'Stream not assigned' ) ;
-  ls := FQuery.ParamByName(pName).Value ;
-  tiStringToStream(ls, pStream);
-end;
-
-procedure TtiQueryZeos.AssignParamFromStream(const pName: string; const pStream : TStream);
-begin
-  Assert( pStream <> nil, 'Stream not assigned' ) ;
-  FQuery.ParamByName( pName ).LoadFromStream( pStream, ftBlob );
 end;
 
 function TtiQueryZeos.HasNativeLogicalType: boolean;
@@ -488,87 +241,14 @@ begin
   result := false ;
 end;
 
-procedure TtiQueryZeos.AssignFieldAsStreamByIndex(pIndex: Integer;const pValue: TStream);
-var
-  lField : TField ;
-begin
-  lField := FQuery.Fields[pIndex];
-  Assert(lField is TBlobField, 'Field index <' + IntToStr(pIndex) + '> not a TBlobField' ) ;
-  ( lField as TBlobField ).SaveToStream(pValue);
-end;
-
-function TtiQueryZeos.GetFieldAsBooleanByIndex(pIndex: Integer): boolean;
-var
-  lsValue : string ;
-begin
-  lsValue := upperCase( FQuery.Fields[ pIndex ].AsString ) ;
-  result := ( lsValue = 'T'    ) or
-            ( lsValue = 'TRUE' ) or
-            ( lsValue = 'Y'    ) or
-            ( lsValue = 'YES'  ) or
-            ( lsValue = '1'    ) ;
-end;
-
-function TtiQueryZeos.GetFieldAsDateTimeByIndex(pIndex: Integer): TDateTime;
-begin
-  result := FQuery.Fields[ pIndex ].AsDateTime ;
-end;
-
-function TtiQueryZeos.GetFieldAsFloatByIndex(pIndex: Integer): extended;
-begin
-  result := FQuery.Fields[ pIndex ].AsFloat ;
-end;
-
-function TtiQueryZeos.GetFieldAsIntegerByIndex(pIndex: Integer): Int64;
-begin
-  result := FQuery.Fields[ pIndex ].AsInteger ;
-end;
-
-function TtiQueryZeos.GetFieldAsStringByIndex(pIndex: Integer): string;
-var
-  lField : TField ;
-  lStream : TStringStream ;
-begin
-  lField := FQuery.Fields[pIndex] ;
-  if lField is TMemoField then
-  begin
-    lStream := TStringStream.Create('') ;
-    try
-      TMemoField(lField).SaveToStream(lStream);
-      lStream.Position := 0 ;
-      result := lStream.DataString ;
-    finally
-      lStream.Free ;
-    end ;
-  end
-  else
-    result := lField.AsString ;
-end;
-
-function TtiQueryZeos.GetFieldIsNullByIndex(pIndex: Integer): Boolean;
-begin
-  result := FQuery.Fields[ pIndex ].IsNull ;
-end;
-
 function TtiDatabaseZeosAbs.Test: boolean;
 begin
-  result := false;
-  Assert( false, 'Under construction' ) ;
+  Result := Connected;
 end;
 
 function TtiDatabaseZeosAbs.TIQueryClass: TtiQueryClass;
 begin
   result:= TtiQueryZeos;
-end;
-
-function TtiQueryZeos.GetParamAsTextBLOB(const psName: string): string;
-begin
-  result := FQuery.ParamByName(psName).AsMemo;
-end;
-
-procedure TtiQueryZeos.SetParamAsTextBLOB(const psName, Value: string);
-begin
-  FQuery.ParamByName(psName).AsMemo := Value;
 end;
 
 end.
