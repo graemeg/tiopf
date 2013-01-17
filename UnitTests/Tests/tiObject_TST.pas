graemeg/tiopf

commit 8f420e3efe931a3b343298566bc872b9b71a9955

* Added a notification system to the Observer implementation in TtiObject and TtiObjcetList. Similar to what TComponent does notification. This allows for a huge speed improvement in the ObjectList mediators. I'm pretty sure tiOPF's GUI components could also use this. * Updated the tiListMediators for LCL components like ListView and StringGrid to optimise redraw using the new notification system.
git-svn-id: https://tiopf.svn.sourceforge.net/svnroot/tiopf/tiOPF2/Trunk@1376 68417047-fa15-0410-9c44-b939d5e0afe3

Core/tiObject.pas
@@ -59,6 +59,9 @@ type
 
   TtiObjectAsDebugStringValuesToShow = set of TtiObjectAsDebugStringValues;
 
+  {: Various observer Notifications }
+  TNotifyOperation = (noChanged,noAddItem,noDeleteItem,noFree,noCustom);
+
 const
   CTIAsDebugStringDataAll =
     [adsData, adsClassName, adsObjectState, adsOID, adsCaption, adsDeleted, adsChildren];
@@ -280,7 +283,6 @@ type
     FObserverList: TList;
     FUpdateCount: Integer;
     function GetObserverList: TList;
-    procedure StopObservers;
   protected
     FOwner: TtiObject; // Want FOwner here as there are time when we may want
                         // go get access without going through the GetOwner and
@@ -449,9 +451,13 @@ type
     {: End a update process }
     procedure   EndUpdate;
     {: Only needed if performing a observing role }
-    procedure   Update(ASubject: TtiObject); virtual;
+    procedure   Update(ASubject: TtiObject); virtual; overload;
+    {: Only needed if performing a observing role where other events than changed need to be observed }
+    procedure   Update(ASubject: TtiObject; AOperation : TNotifyOperation); virtual; overload;
     {: Notify all the attached observers about a change }
-    procedure   NotifyObservers; virtual;
+    procedure   NotifyObservers; virtual; overload;
+    {: Notify all the attached observers about a change operation}
+    procedure   NotifyObservers(ASubject : TTiObject; AOperation : TNotifyOperation); virtual; overload;
     {: Used to get access to the internal observer list. This has been surfaced
        so that the MGM List Views can atttach/detach observers to the selected
        object. Not a great way of doing it - we need a different design. }
@@ -1297,6 +1303,7 @@ begin
     if FbAutoSetItemOwner then
       AObject.Owner := FItemOwner;
     result := FList.Add(AObject);
+    NotifyObservers(AObject,noAddItem);
   Finally
     EndUpdate;
   end;
@@ -1332,6 +1339,7 @@ procedure TtiObjectList.Delete(i: integer);
 begin
   BeginUpdate;
   try
+    NotifyObservers(TtiObject(Items[i]),noDeleteItem);
     if AutoSetItemOwner then
       TtiObject(Items[i]).Owner := nil;
     FList.Delete(i);
@@ -1495,6 +1503,7 @@ function TtiObjectList.Remove(const AObject: TtiObject):integer;
 begin
   BeginUpdate;
   Try
+    NotifyObservers(AObject,noDeleteItem);
     if AutoSetItemOwner then
       AObject.Owner := nil;
     result := FList.Remove(AObject);
@@ -1510,6 +1519,7 @@ procedure TtiObjectList.Extract(const AObject: TtiObject);
 begin
   BeginUpdate;
   Try
+    NotifyObservers(AObject,noDeleteItem);
     if AutoSetItemOwner then
       AObject.Owner := nil;
     FList.Extract(AObject);
@@ -1531,6 +1541,7 @@ begin
     FList.Insert(AIndex, AObject);
     if FbAutoSetItemOwner then
       AObject.Owner := FItemOwner;
+    NotifyObservers(AObject,noAddItem);
   Finally
     EndUpdate;
   end;
@@ -2763,8 +2774,7 @@ begin
   {$IFNDEF OID_AS_INT64}
     FOID.Free;
   {$ENDIF}
-  if (FObserverList<>Nil) then
-    StopObservers;
+  NotifyObservers(Self,noFree);
   FreeAndNil(FObserverList);
   inherited;
 end;
@@ -3895,6 +3905,12 @@ begin
 end;
 
 procedure TtiObject.NotifyObservers;
+begin
+  NotifyObservers(Self,noChanged);
+end;
+
+procedure TtiObject.NotifyObservers(ASubject: TTiObject;
+    AOperation: TNotifyOperation);
 var
   ObjectIndex: Integer;
   Observer: TtiObject;
@@ -3906,13 +3922,21 @@ begin
   begin
     Observer := TtiObject(FObserverList.Items[ObjectIndex]);
     if Assigned(Observer) then
-      Observer.Update(self);
+      Observer.Update(ASubject,AOperation);
   end;
 end;
 
 procedure TtiObject.Update(ASubject: TtiObject);
 begin
   { Do nothing here. This will be implemented in decendant classes when needed }
+end;
+
+procedure TtiObject.Update(ASubject: TtiObject; AOperation: TNotifyOperation);
+begin
+  If (AOperation=noChanged) then
+    Update(ASubject)
+  else if (AOperation=noFree) then
+    StopObserving(ASubject)
 end;
 
 procedure TtiObject.StopObserving(ASubject: TtiObject);
@@ -3925,20 +3949,6 @@ begin
   if not Assigned(FObserverList) then
     FObserverList := TList.Create;
   Result := FObserverList;
-end;
-
-procedure TtiObject.StopObservers;
-var
-  I: Integer;
-  O: TtiObject;
-begin
-  if Assigned(FObserverList) then
-    for I := FObserverList.Count-1 downto 0 do
-    begin
-      O := TTiObject(FObserverList[i]);
-      if Assigned(O) then
-        O.StopObserving(Self);
-    end;
 end;
 
 procedure TtiObjectList.FreeDeleted;

Compilers/FPC/tiopflcl.lpk
@@ -2,6 +2,7 @@
 <CONFIG>
   <Package Version="3">
     <Name Value="tiopfLCL"/>
+    <Author Value="Graeme &amp; Michael"/>
     <CompilerOptions>
       <Version Value="8"/>
       <SearchPaths>
@@ -17,7 +18,10 @@
         <CompilerPath Value="$(CompPath)"/>
       </Other>
     </CompilerOptions>
-    <Description Value="Lazarus specific GUI support"/>
+    <Description Value="Lazarus specific GUI support
+"/>
+    <License Value="Mozilla Public License 1.1 or Modified LGPL"/>
+    <Version Major="2" Release="4"/>
     <Files Count="2">
       <Item1>
         <Filename Value="../../GUI/LCL/tiMediators.pas"/>

Core/tiBaseMediator.pas
@@ -19,6 +19,7 @@ uses
 type
   // forward declaration
   TMediatorView = class;
+  TCustomListMediator = Class;
 
   TObjectToGUIEvent = procedure(Sender: TMediatorView; Src: TtiObject; Dest: TComponent; var Handled: Boolean) of object;
   TGUIToObjectEvent = procedure(Sender: TMediatorView; Src: TComponent; Dest: TtiObject; var Handled: Boolean) of object;
@@ -90,7 +91,7 @@ type
     // Copy GUI to Object. Calls OnObjectToGUI if set, and then calls DoGUIToObject if needed
     procedure ObjectToGui;
     // Called by NotifyObservers of subject. Calls ObjectToGUI by default.
-    procedure Update(pSubject: TtiObject); override;
+    procedure   Update(pSubject: TtiObject; AOperation : TNotifyOperation); override;
     // Call when GUI changed. Will call GUIToObject.
     procedure GUIChanged;
     // Access properties.
@@ -146,9 +147,13 @@ type
 
   TtiMediatorFieldInfoList = class(TCollection)
   private
+    FMediator : TCustomListMediator;
     function GetAsString: string;
     function GetI(Index: integer): TtiMediatorFieldInfo;
     procedure SetI(Index: integer; const AValue: TtiMediatorFieldInfo);
+  protected
+    procedure Notify(Item: TCollectionItem;Action: TCollectionNotification); override;
+    Property Mediator : TCustomListMediator read FMediator;
   public
     function AddFieldInfo: TtiMediatorFieldInfo; overload;
     function AddFieldInfo (Const APropName : String; AFieldWidth : Integer) : TtiMediatorFieldInfo; overload;
@@ -201,11 +206,13 @@ type
     procedure SetShowDeleted(const AValue: Boolean);
     procedure SetOnBeforeSetupField(const Value: TOnBeforeSetupField);
   protected
+    procedure FieldInfoChanged(Item: TtiMediatorFieldInfo;Action: TCollectionNotification); virtual;
     function GetSelectedObject: TtiObject; virtual;
     procedure SetSelectedObject(const AValue: TtiObject); virtual;
     procedure CreateColumns; virtual; abstract;
     procedure ClearList; virtual; abstract;
     procedure DoCreateItemMediator(AData: TtiObject; ARowIdx: integer); virtual; abstract;
+    procedure DoDeleteItemMediator(AIndex : Integer; AMediator : TListItemMediator); virtual;
     procedure ParseDisplayNames(const AValue: string);
     procedure CreateSubMediators; virtual;
     procedure RebuildList; virtual; abstract;
@@ -215,10 +222,12 @@ type
     procedure SetSubject(const AValue: TtiObject); override;
     procedure SetFieldName(const AValue: string); override;
     procedure SetActive(const AValue: Boolean); override;
+    Function FindObjectMediator(AObject : TTiObject; Var AtIndex : Integer) : TListItemMediator;
     property MediatorList: TObjectList read FMediatorList;
   public
     constructor Create; override;
     class function CompositeMediator: Boolean; override;
+    procedure Update(ASubject: TtiObject; AOperation : TNotifyOperation); override;
     procedure HandleSelectionChanged; virtual; // Called from the GUI to trigger events
     property SelectedObject: TtiObject read GetSelectedObject write SetSelectedObject;
   published
@@ -301,6 +310,7 @@ resourcestring
   sErrInvalidWidthSpecifier = 'Invalid with "%s" specified for column %d';
   sErrNotListObject         = '%s is not a TTiListObject';
   sErrCompositeNeedsList    = '%s needs a TtiObjectList class but is registered with %s';
+  SErrActive                = 'Operation not allowed while the mediator is active';
 
 const
   DefFieldWidth = 75;   // default width
@@ -454,11 +464,14 @@ begin
   CheckSetupGUIAndObject;
 end;
 
-procedure TMediatorView.Update(pSubject: TtiObject);
+procedure TMediatorView.Update(pSubject: TtiObject; AOperation : TNotifyOperation);
 begin
   inherited;
-  ObjectToGui;
-  TestIfValid;
+  if AOperation=noChanged then
+  begin
+    ObjectToGui;
+    TestIfValid;
+  end;
 end;
 
 function TMediatorView.GetSubject: TtiObject;
@@ -897,6 +910,14 @@ begin
   Items[Index] := AValue;
 end;
 
+procedure TtiMediatorFieldInfoList.Notify(Item: TCollectionItem;
+  Action: TCollectionNotification);
+begin
+  inherited Notify(Item, Action);
+  If Assigned(FMediator) and FMediator.Active then
+    FMediator.FieldInfoChanged(Item as TtiMediatorFieldInfo,Action)
+end;
+
 function TtiMediatorFieldInfoList.AddFieldInfo: TtiMediatorFieldInfo;
 begin
   Result := Add as TtiMediatorFieldInfo;
@@ -987,6 +1008,13 @@ begin
     TListItemMediator(FMediatorList[i]).OnBeforeSetupField := Value;
 end;
 
+procedure TCustomListMediator.FieldInfoChanged(Item: TtiMediatorFieldInfo;
+  Action: TCollectionNotification);
+begin
+  If Active  then
+    Raise EMediator.Create(SErrActive);
+end;
+
 procedure TCustomListMediator.SetSubject(const AValue: TtiObject);
 begin
   if (AValue <> nil) then
@@ -1008,6 +1036,18 @@ begin
   inherited SetActive(AValue);
   for I := 0 to FMediatorList.Count - 1 do
     TListItemMediator(FMediatorList[i]).Active := AValue;
+end;
+
+function TCustomListMediator.FindObjectMediator(AObject: TTiObject;
+  var AtIndex: Integer): TListItemMediator;
+begin
+  AtIndex:=FMediatorList.Count-1;
+  While (ATIndex>=0) and (TListItemMediator(FMediatorList[AtIndex]).Model<>AObject) do
+    Dec(AtIndex);
+  If (AtIndex=-1) then
+    Result:=Nil
+  else
+    Result:=TListItemMediator(FMediatorList[AtIndex]);
 end;
 
 function TCustomListMediator.GetModel: TtiObjectList;
@@ -1050,6 +1090,11 @@ begin
   // Do nothing
 end;
 
+procedure TCustomListMediator.DoDeleteItemMediator(AIndex : Integer; AMediator : TListItemMediator);
+begin
+  MediatorList.Delete(AIndex);
+end;
+
 procedure TCustomListMediator.SetShowDeleted(const AValue: Boolean);
 begin
   if FShowDeleted = AValue then
@@ -1077,7 +1122,7 @@ begin
         DoCreateItemMediator(Model.Items[i], i);
     end;
   for I:=MediatorList.Count-1 downto Model.Count do
-    MediatorList.Delete(I);
+    DoDeleteItemMediator(I,TListItemMediator(MediatorList[i]));
 end;
 
 function TCustomListMediator.DataAndPropertyValid(const AData: TtiObject): Boolean;
@@ -1115,6 +1160,7 @@ constructor TCustomListMediator.Create;
 begin
   inherited Create;
   FFieldsInfo   := TtiMediatorFieldInfoList.Create(TtiMediatorFieldInfo);
+  FFieldsInfo.FMediator:=Self;
   FMediatorList := TObjectList.Create;
   FShowDeleted  := False;
   Active        := False;
@@ -1123,6 +1169,25 @@ end;
 class function TCustomListMediator.CompositeMediator: Boolean;
 begin
   Result := True;
+end;
+
+procedure TCustomListMediator.Update(ASubject: TtiObject;
+  AOperation: TNotifyOperation);
+var
+  M : TListItemMediator;
+  I : Integer;
+begin
+  // Do not call inherited, it will rebuild the list !!
+  Case AOperation of
+    noAddItem    : DoCreateItemMediator(ASubject,Model.Count); // always at the end...
+    noDeleteItem : begin
+                   M:=FindObjectmediator(ASubject,I);
+                   If M<>Nil then
+                     DoDeleteItemMediator(I,M);
+                   end;
+    noChanged    : If (Model.Count<>MediatorList.Count) or (Model.Count=0) then // Safety measure
+                     RebuildList
+  end;
 end;
 
 procedure TCustomListMediator.HandleSelectionChanged;

GUI/LCL/tiListMediators.pas
@@ -30,6 +30,7 @@ type
     procedure SetSelectedObject(const AValue: TtiObject); override;
     procedure CreateColumns; override;
     procedure DoCreateItemMediator(AData: TtiObject; ARowIdx: integer); override;
+    procedure DoDeleteItemMediator(AIndex : Integer; AMediator : TListItemMediator); override;
     function GetGuiControl: TComponent; override;
     procedure SetGuiControl(const AValue: TComponent); override;
     procedure SetupGUIandObject; override;
@@ -58,6 +59,7 @@ type
   private
     FView: TStringGrid;
     procedure DoCreateItemMediator(AData: TtiObject; ARowIdx: integer); override;
+    procedure DoDeleteItemMediator(AIndex : Integer; AMediator : TListItemMediator); override;
     procedure SetView(const AValue: TStringGrid);
   protected
     function GetSelectedObject: TtiObject; override;
@@ -66,6 +68,7 @@ type
     function GetGuiControl: TComponent; override;
     procedure SetGuiControl(const AValue: TComponent); override;
     procedure SetupGUIandObject; override;
+    procedure ClearList; override;
     procedure RebuildList; override;
   public
     constructor CreateCustom(AModel: TtiObjectList; AGrid: TStringGrid; ADisplayNames: string; AIsObserving: Boolean = True);
@@ -169,10 +172,15 @@ var
 begin
   DataAndPropertyValid(AData);
   { Create ListItem and Mediator }
-  li:=FView.Items.Add;
-  m:= TListViewListItemMediator.CreateCustom(AData, li, OnBeforeSetupField, FieldsInfo, Active);
-  li.Data := M;
-  MediatorList.Add(m);
+  FView.BeginUpdate;
+  try
+    li:=FView.Items.Add;
+    m:= TListViewListItemMediator.CreateCustom(AData, li, OnBeforeSetupField, FieldsInfo, Active);
+    li.Data := M;
+    MediatorList.Add(m);
+  Finally
+    FView.EndUpdate;
+  end;
 end;
 
 procedure TListViewMediator.CreateColumns;
@@ -181,7 +189,9 @@ var
   lc: TListColumn;
   lInfo: TtiMediatorFieldInfo;
 begin
-  if View.Columns.Count = 0 then
+  If (View.Columns.Count<>FieldsInfo.Count) then
+    View.Columns.Clear;
+  if View.Columns.Count=0 then
   begin
     for c := 0 to FieldsInfo.Count-1 do
     begin
@@ -217,14 +227,11 @@ end;
 
 procedure TListViewMediator.RebuildList;
 begin
-  ClearList;
-
   { This rebuilds the whole list. Not very efficient. You can always override
     this in your mediators to create a more optimised rebuild. }
   View.BeginUpdate;
   try
-    View.Columns.Clear;
-    View.Items.Clear;
+    CreateColumns;
     CreateSubMediators;
   finally
     View.EndUpdate;
@@ -236,6 +243,13 @@ begin
   if not AValue then
     ClearList;
   inherited SetActive(AValue);
+end;
+
+procedure TListViewMediator.DoDeleteItemMediator(AIndex: Integer;
+  AMediator: TListItemMediator);
+begin
+  FView.Items.Delete(TListViewListItemMediator(AMediator).FView.Index);
+  inherited DoDeleteItemMediator(AIndex, AMediator);
 end;
 
 constructor TListViewMediator.CreateCustom(AModel: TtiObjectList; AView: TListView; AOnBeforeSetupField: TOnBeforeSetupField; ADisplayNames: string; AIsObserving: Boolean);
@@ -420,14 +434,28 @@ var
   lFieldName: string;
   lMediatorView: TStringGridRowMediator;
 begin
-  for i := 0 to FieldsInfo.Count - 1 do
-  begin
-    lFieldName := FieldsInfo[i].PropName;
-    FView.Cells[i, ARowIdx+1] := AData.PropValue[lFieldName];  // set Cell text
+  FView.BeginUpdate;
+  try
+    If ARowIdx=FView.RowCount then // In case of add notification
+      FView.RowCount:=FView.RowCount+1;
+    for i := 0 to FieldsInfo.Count - 1 do
+    begin
+      lFieldName := FieldsInfo[i].PropName;
+      FView.Cells[i, ARowIdx] := AData.PropValue[lFieldName];  // set Cell text
+    end;
+    lMediatorView := TStringGridRowMediator.CreateCustom(AData, FView, FieldsInfo, ARowIdx+1, Active);
+    FView.Objects[0, ARowIdx] := lMediatorView;   // set Object reference inside grid
+    MediatorList.Add(lMediatorView);
+  Finally
+    FView.EndUpdate;
   end;
-  lMediatorView := TStringGridRowMediator.CreateCustom(AData, FView, FieldsInfo, ARowIdx+1, Active);
-  FView.Objects[0, ARowIdx+1] := lMediatorView;   // set Object reference inside grid
-  MediatorList.Add(lMediatorView);
+end;
+
+procedure TStringGridMediator.DoDeleteItemMediator(AIndex: Integer;
+  AMediator: TListItemMediator);
+begin
+  FView.DeleteColRow(False,AIndex+1);
+  Inherited DoDeleteItemMediator(AIndex,AMediator);
 end;
 
 procedure TStringGridMediator.CreateColumns;
@@ -466,6 +494,13 @@ begin
   FView.Options:=FView.Options+[goRowSelect];
   // Rowcount is set after columns are created, because clearing columns
   //  resets rowcount.
+end;
+
+procedure TStringGridMediator.ClearList;
+begin
+  MediatorList.Clear;
+  If Assigned(View) then
+    View.RowCount:=1;
 end;
 
 procedure TStringGridMediator.RebuildList;

commit 2721f3445cbefcddea2d45020a4d23fb5ca52e43

Fixes a NotifyObservers bug introduced in r1376.
Even when we add, remove, extract etc objects from a objectlist, the
Subject in the NotifyObservers call should always be the objectlist,
not the item being added, removed etc. This is as per the Observer
documentation in the GoF book.

I added some new unit tests to confirm this change.

git-svn-id: https://tiopf.svn.sourceforge.net/svnroot/tiopf/tiOPF2/Trunk@2303 68417047-fa15-0410-9c44-b939d5e0afe3

Core/tiObject.pas
@@ -1436,7 +1436,7 @@ begin
   if FbAutoSetItemOwner then
     AObject.Owner := FItemOwner;
   result := FList.Add(AObject);
-  NotifyObservers(AObject,noAddItem);
+  NotifyObservers(self, noAddItem);
 end;
 
 procedure TtiObjectList.Clear;
@@ -1463,7 +1463,7 @@ end;
   Extract. }
 procedure TtiObjectList.Delete(i: integer);
 begin
-  NotifyObservers(TtiObject(Items[i]),noDeleteItem);
+  NotifyObservers(self, noDeleteItem);
   if AutoSetItemOwner then
     TtiObject(Items[i]).Owner := nil;
   FList.Delete(i);
@@ -1628,7 +1628,7 @@ end;
   without freeing it, call Extract. }
 function TtiObjectList.Remove(const AObject: TtiObject):integer;
 begin
-  NotifyObservers(AObject,noDeleteItem);
+  NotifyObservers(self, noDeleteItem);
   if AutoSetItemOwner then
     AObject.Owner := nil;
   result := FList.Remove(AObject);
@@ -1639,7 +1639,7 @@ end;
   up in index position and Count is decremented.}
 procedure TtiObjectList.Extract(const AObject: TtiObject);
 begin
-  NotifyObservers(AObject,noDeleteItem);
+  NotifyObservers(self, noDeleteItem);
   if AutoSetItemOwner then
     AObject.Owner := nil;
   FList.Extract(AObject);
@@ -1656,7 +1656,7 @@ begin
   FList.Insert(AIndex, AObject);
   if FbAutoSetItemOwner then
     AObject.Owner := FItemOwner;
-  NotifyObservers(AObject,noAddItem);
+  NotifyObservers(self, noAddItem);
 end;
 
 procedure TtiObjectList.Insert(const AInsertBefore: TtiObject; const AObject: TtiObject);

UnitTests/Tests/tiObject_TST.pas
@@ -174,6 +174,9 @@ type
 {$IFDEF DELPHI9ORABOVE}
     procedure   ForIn;
 {$ENDIF}
+    procedure   Add_NotifyObservers_Subject;
+    procedure   Remove_NotifyObservers_Subject;
+    procedure   BeginEnd_NotifyObservers_Subject;
   end;
 
 
@@ -280,12 +283,14 @@ type
   // Used in NotifyObserver test.
   TtstObserver2 = class(TtiObject)
   private
+    FLastUpdateSubject: TtiObject;
     FLastNotifyOperation: TNotifyOperation;
     FUpdateCount: integer;
   public
     constructor Create; override;
     procedure Update(ASubject: TtiObject; AOperation: TNotifyOperation); override;
     property LastNotifyOperation: TNotifyOperation read FLastNotifyOperation write FLastNotifyOperation;
+    property LastUpdateSubject: TtiObject read FLastUpdateSubject write FLastUpdateSubject;
     property UpdateCount: integer read FUpdateCount write FUpdateCount;
   end;
 
@@ -3731,6 +3736,93 @@ begin
   end;
 end;
 
+procedure TtiObjectListTestCase.Add_NotifyObservers_Subject;
+var
+  lList: TtiObjectList;
+  lItem: TtiObject;
+  lObserver: TtstObserver2;
+begin
+  lList := TtiObjectList.Create;
+  lItem := TtiObject.Create;
+  lObserver := TtstObserver2.Create;
+
+  lList.AttachObserver(lObserver);
+  { simply testing the default value - first item in TNotifyOperation enum }
+  CheckNotifyOperation(noCustom, lObserver.LastNotifyOperation, 'Failed on 1');
+  CheckEquals(0, lObserver.UpdateCount, 'Failed on 2');
+  CheckTrue(lObserver.LastUpdateSubject = nil, 'Failed on 3');
+
+  lList.Add(lItem);
+  CheckEquals(1, lObserver.UpdateCount, 'Failed on 4');
+  CheckNotifyOperation(noAddItem, lObserver.LastNotifyOperation, 'Failed on 5');
+  { The Subject in Update() should be the list, not the item added }
+  CheckTrue(lObserver.LastUpdateSubject = lList, 'Failed on 6');
+
+  lList.Remove(lItem);
+  lList.Free;
+  lObserver.Free;
+end;
+
+procedure TtiObjectListTestCase.Remove_NotifyObservers_Subject;
+var
+  lList: TtiObjectList;
+  lItem: TtiObject;
+  lObserver: TtstObserver2;
+begin
+  lList := TtiObjectList.Create;
+  lItem := TtiObject.Create;
+  lObserver := TtstObserver2.Create;
+
+  lList.AttachObserver(lObserver);
+  { simply testing the default value - first item in TNotifyOperation enum }
+  CheckNotifyOperation(noCustom, lObserver.LastNotifyOperation, 'Failed on 1');
+  CheckEquals(0, lObserver.UpdateCount, 'Failed on 2');
+  CheckTrue(lObserver.LastUpdateSubject = nil, 'Failed on 3');
+
+  lList.Add(lItem);
+  { Reset last values }
+  lObserver.UpdateCount := 0;
+  lObserver.LastUpdateSubject := nil;
+
+  lList.Remove(lItem);
+  CheckEquals(1, lObserver.UpdateCount, 'Failed on 4');
+  CheckNotifyOperation(noDeleteItem, lObserver.LastNotifyOperation, 'Failed on 5');
+  { The Subject in Update() should be the list, not the item added }
+  CheckTrue(lObserver.LastUpdateSubject = lList, 'Failed on 6');
+
+  lList.Free;
+  lObserver.Free;
+end;
+
+procedure TtiObjectListTestCase.BeginEnd_NotifyObservers_Subject;
+var
+  lList: TtiObjectList;
+  lItem: TtiObject;
+  lObserver: TtstObserver2;
+begin
+  lList := TtiObjectList.Create;
+  lItem := TtiObject.Create;
+  lObserver := TtstObserver2.Create;
+
+  lList.AttachObserver(lObserver);
+  { simply testing the default value - first item in TNotifyOperation enum }
+  CheckNotifyOperation(noCustom, lObserver.LastNotifyOperation, 'Failed on 1');
+  CheckEquals(0, lObserver.UpdateCount, 'Failed on 2');
+  CheckTrue(lObserver.LastUpdateSubject = nil, 'Failed on 3');
+
+  lList.BeginUpdate;
+  lList.Add(lItem);
+  lList.EndUpdate;
+  CheckEquals(2, lObserver.UpdateCount, 'Failed on 4');
+  CheckNotifyOperation(noChanged, lObserver.LastNotifyOperation, 'Failed on 5');
+  { The Subject in Update() should be the list, not the item added }
+  CheckTrue(lObserver.LastUpdateSubject = lList, 'Failed on 6');
+
+  lList.Remove(lItem);
+  lList.Free;
+  lObserver.Free;
+end;
+
 procedure TtiObjectListTestCase.tiListToStreamDefault;
 var
   lStream : TStringStream;
@@ -4816,6 +4908,7 @@ end;
 procedure TtstObserver2.Update(ASubject: TtiObject; AOperation: TNotifyOperation);
 begin
   Inc(FUpdateCount);
+  FLastUpdateSubject := ASubject;
   FLastNotifyOperation := AOperation;
   inherited Update(ASubject, AOperation);
 end;