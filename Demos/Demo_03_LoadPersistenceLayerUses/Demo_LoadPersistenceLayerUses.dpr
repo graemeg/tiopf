3c26fe633bba6f5bd6346e3d3c65519f063d214d
Graeme Geldenhuys <graemeg@gmail.com>
2015-06-11 12:40:54 +0100

    demo: fixes to make it cross-platform enabled
    * fixes spelling mistakes
    * adds missing tiDefines.inc include file
    * IFDEF persistence layers not applicable to non-Windows platforms

--- Demos/Demo_03_LoadPersistenceLayerUses/Demo_LoadPersistenceLayerUses.dpr
+++ Demos/Demo_03_LoadPersistenceLayerUses/Demo_LoadPersistenceLayerUses.dpr
@@ -1,18 +1,26 @@
 program Demo_LoadPersistenceLayerUses;
 
+{$I tiDefines.inc}
+
 // Adding a tiQueryXXX unit to your project will force that
 // persistence layer to be loaded.
 // When you run this demo, a dialog will show listing
 // the loaded persistence layers. (Note, the dialog will
-// say the database is not connected - wich is correct.)
+// say the database is not connected - which is correct.)
 uses
-  DemoDBUtils in '..\Common\DemoDBUtils.pas',
-  tiQueryIBX,
-  tiQueryXML,
-  tiQueryXMLLight,
-  tiQueryCSV,
-  tiQueryADOAccess
- ;
+  DemoDBUtils in '..\Common\DemoDBUtils.pas'
+  {$IFDEF FPC}
+  ,tiQuerySqldbIB
+  {$ELSE}
+  ,tiQueryIBX
+  {$ENDIF}
+  ,tiQueryXMLLight
+  ,tiQueryCSV
+  {$IFDEF MSWINDOWS}
+  ,tiQueryXML
+  ,tiQueryADOAccess
+  {$ENDIF}
+  ;
 
 {$R *.res}
 
