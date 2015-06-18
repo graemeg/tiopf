6476a1e6d730dab9c4497f502bdd227ef77d0b44
Graeme Geldenhuys <graemeg@gmail.com>
2015-06-18 12:42:02 +0100

    tiOPF.lpk package file changes.
    No idea what Lazarus IDE actually changed (I think SQLite3 PL), but lets
    commit it anyway.

--- Compilers/FPC/tiOPF.pas
+++ Compilers/FPC/tiOPF.pas
@@ -2,22 +2,20 @@
   This source is only used to compile and install the package.
  }
 
-unit tiOPF; 
+unit tiOPF;
 
 interface
 
 uses
-    tiBaseObject, tiCommandLineParams, tiCompress, tiConstants, tiDBConnectionPool, 
-  tiEncrypt, tiExcept, tiLog, tiObject, tiOID, tiOPFManager, tiPool, tiQuery, tiStreams, 
-  tiSyncObjs, tiThread, tiUtils, tiVisitor, tiVisitorDB, tiOIDGUID, tiOIDInteger, 
-  tiCompressNone, tiCompressZLib, tiEncryptSimple, tiEncryptNone, tiEncryptDES, 
-  tiEncryptBlowfish, tiOIDString, tiDataBuffer_Cli, tiDataBuffer_BOM, tiQueryTXTAbs, 
-  tiTextParser, tiLogToConsole, tiXML, tiQueryCSV, tiQueryTAB, tiLogToFile, tiCriteria, 
-  tiVisitorCriteria, tiXMLToTIDataset, tiQueryXMLLight, tiRTTI, tiTokenLibrary, tiINI, 
-  tiQueue, tiAutoMap, tiVisitorDBAutoGen, tiStructuredCSVReader, tiStructuredCSVWriter, 
-  tiFilteredObjectList, tiBaseMediator, tiModelMediator, tiQueryDataset, tiOPFSqlScript, 
-  tiOIDInt64, tiLogToDebugSvr, tiPersistenceLayers, tiWebServerClientConnectionDetails, 
-  tiCRC32, tiHTTP;
+  tiBaseObject, tiCommandLineParams, tiCompress, tiConstants, tiDBConnectionPool, tiEncrypt, tiExcept, 
+  tiLog, tiObject, tiOID, tiOPFManager, tiPool, tiQuery, tiStreams, tiSyncObjs, tiThread, tiUtils, 
+  tiVisitor, tiVisitorDB, tiOIDGUID, tiOIDInteger, tiCompressNone, tiCompressZLib, tiEncryptSimple, 
+  tiEncryptNone, tiEncryptDES, tiEncryptBlowfish, tiOIDString, tiDataBuffer_Cli, tiDataBuffer_BOM, 
+  tiQueryTXTAbs, tiTextParser, tiLogToConsole, tiXML, tiQueryCSV, tiQueryTAB, tiLogToFile, tiCriteria, 
+  tiVisitorCriteria, tiXMLToTIDataset, tiQueryXMLLight, tiRTTI, tiTokenLibrary, tiINI, tiQueue, tiAutoMap, 
+  tiVisitorDBAutoGen, tiStructuredCSVReader, tiStructuredCSVWriter, tiFilteredObjectList, tiBaseMediator, 
+  tiModelMediator, tiQueryDataset, tiOPFSqlScript, tiOIDInt64, tiLogToDebugSvr, tiPersistenceLayers, 
+  tiWebServerClientConnectionDetails, tiCRC32, tiHTTP, tiQuerySqldb, tiQuerySqldbIB, tiQuerySqldbSQLite3;
 
 implementation
 
