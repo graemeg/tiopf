8656c97d7e7a352ce3112a7f5b4b3c4ecb983a6e
Graeme Geldenhuys <graemeg@gmail.com>
2015-06-25 20:19:23 +0100

    minor code formatting

--- Core/tiBaseMediator.pas
+++ Core/tiBaseMediator.pas
@@ -1442,8 +1442,9 @@ var
 begin
   CreateColumns;
   for i := 0 to Model.Count - 1 do
-    begin
+  begin
     if (not Model.Items[i].Deleted) or FShowDeleted then
+    begin
       if i < MediatorList.Count then
         TtiListItemMediator(MediatorList[i]).Model := Model.Items[i]
       else
@@ -1452,6 +1453,7 @@ begin
         LItemMediator.ListMediator := Self;
       end;
     end;
+  end;
   for i := MediatorList.Count-1 downto Model.Count do
     DoDeleteItemMediator(I,TtiListItemMediator(MediatorList[i]));
   FListChanged:=False;
