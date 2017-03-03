graemeg/tiopf

commit be194c1d655637389b15003780ac4cccda3ebdbf

mediators: fix issue with added/deleted items and list mediators.
The Problem:
If you had a list with 2 items, then used BeginUpdate..EndUpdate
around the list actions - deleted existing two items, and added
two new ones - then the ListMediator View did not update. You ended
up still showing the original two items - which are now actually
marked deleted.

Core/tiBaseMediator.pas
@@ -251,7 +251,6 @@ type
     procedure SetShowDeleted(const AValue: Boolean);
     procedure SetOnBeforeSetupField(const Value: TtiOnBeforeSetupField);
   protected
-    FListChanged : Boolean;
     FFieldsInfo: TtiMediatorFieldInfoList;
     procedure FieldInfoChanged(Item: TtiMediatorFieldInfo;Action: TCollectionNotification); virtual;
     procedure CreateColumns; virtual; abstract;
@@ -1285,21 +1284,16 @@ begin
 end;
 
 procedure TtiListItemMediator.SetModel(const AValue: TtiObject);
-var
-  B : Boolean;
 begin
   if AValue=FModel then
     Exit;
-  B := Assigned(FModel);
-  if B then
+  if Assigned(FModel) then
     FModel.DetachObserver(Self);
   FModel := AValue;
   if Assigned(FModel) then
-  begin
     FModel.AttachObserver(Self);
-    if B and Active then
-      Update(FModel,noChanged, nil);
-  end;
+  if Active then
+    Update(FModel,noChanged, nil);
 end;
 
 procedure TtiListItemMediator.StopObserving(ASubject: TtiObject);
@@ -1352,7 +1346,6 @@ begin
   end
   else
     ClearList;
-  FListChanged:=True;
   inherited SetSubject(AValue);
 end;
 
@@ -1448,7 +1441,7 @@ begin
     if (not Model.Items[i].Deleted) or FShowDeleted then
     begin
       inc(idx);
-      if i < MediatorList.Count then
+      if idx < MediatorList.Count then
         TtiListItemMediator(MediatorList[idx]).Model := Model.Items[i]
       else
       begin
@@ -1457,9 +1450,8 @@ begin
       end;
     end;
   end;
-  for i := MediatorList.Count-1 downto Model.Count do
+  for i := MediatorList.Count-1 downto idx+1 do
     DoDeleteItemMediator(I,TtiListItemMediator(MediatorList[i]));
-  FListChanged:=False;
 end;
 
 function TtiCustomListMediatorView.DataAndPropertyValid(const AData: TtiObject): Boolean;
@@ -1520,27 +1512,15 @@ var
   M: TtiListItemMediator;
 begin
   // Do not call inherited, it will rebuild the list !!
-  Case AOperation of
+  case AOperation of
     noAddItem    : begin
-                     M := DoCreateItemMediator(AData,Model.Count-1); // always at the end...
+                     M := DoCreateItemMediator(AData, Model.Count-1); // always at the end...
                      M.ListMediator := Self;
                    end;
     noDeleteItem : ItemDeleted(AData);
     noFree       : if (ASubject = FSubject) then
                      Subject := nil;
-    noChanged    :
-                  begin
-                    if ShowDeleted then
-                    begin
-                      if FListChanged or (Model.Count<>MediatorList.Count) or (Model.Count=0) then // Safety measure
-                       RebuildList;
-                    end
-                    else
-                    begin
-                      if FListChanged or (Model.CountNotDeleted<>MediatorList.Count) or (Model.CountNotDeleted=0) then // Safety measure
-                       RebuildList;
-                    end;
-                  end;
+    noChanged    : RebuildList;
     noReSort     : RebuildList;
   end;
 end;

Demos/LCL/Demo_20_ListMediators/frmMain.pas
@@ -107,7 +107,6 @@ end;
 procedure TForm1.btnDeleteClick(Sender: TObject);
 var
   med: TtiMediatorView;
-  i: integer;
 begin
   med := FMediator.FindByComponent(grdName1).Mediator;
   if not Assigned(TtiStringGridMediatorView(med).SelectedObject) then

commit a34e7bab832b73d9ea73cdae95f4d0f7baf6dee9

mediators: fix a bug introduced by be194c1d.
We called Update() when FModel = nil, which doesn't make
much sense.

Core/tiBaseMediator.pas
@@ -1312,7 +1312,7 @@ begin
   FModel := AValue;
   if Assigned(FModel) then
     FModel.AttachObserver(Self);
-  if Active then
+  if Active and Assigned(FModel) then
     Update(FModel,noChanged, nil);
 end;
 